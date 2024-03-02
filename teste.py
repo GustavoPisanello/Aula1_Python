f3080d12d917a76d83e04672ee688959daafc59d
GustavoPisanello <99992149+GustavoPisanello@users.noreply.github.com>
2024-03-01 21:33:49 -0300

    Aula 1 - Python
    Programa que calcula média aritmética das notas

--- teste.py
+++ teste.py
@@ -1,14 +1,53 @@
 avrg = []
-def inputGrades():
+def NumberOfGrades():
+    """Selects the number of grades to be used in calculation
+    
+    Input:
+    - The number of grades that will be used in the calculation of average
+    """
+    qty = int(input("Digite o número de notas que deseja realizar a média: "))
+    InputGrades(qty)
+
+def InputGrades(qty):
+    """Inserts the grades istelf
+    
+    Input:
+    - Grades (int)
+
+    Parameters:
+    - qty (quantity of grades - int)
+    """
     i = 0
-    while i < 3:
+    while i < qty:
         grade = int(input(f"Digite a {i + 1}° Nota: "))
         i += 1
         avrg.append(grade)
-    Average()
+    Average(qty)
 
-def Average():
-    result = (sum(avrg)/3)
-    print(result)
+def Average(qty):
+    """Calculates the average grade based on the number of grades
+    
+    Output:
+    - Result (The avarage itself - int)
 
-inputGrades()
+    Parameter:
+    - qty (quantity of grades - int)
+    """
+    result = round((sum(avrg)/qty), 1)
+    Situation(result)
+
+def Situation(result):
+    """Informs the actual situation
+    
+    Parameter:
+    - Result (The average itself - int)
+
+    Output:
+    - The user's situation
+    """
+    if result > 7:
+        print(f"Sua nota é {result}! Você foi aprovado! :)")
+    else: 
+        print(f"Sua nota é {result}. Você foi reprovado :(")
+
+NumberOfGrades()
